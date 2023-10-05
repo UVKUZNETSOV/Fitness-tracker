dc0a26d993802a6c7498e6bec5eb1a07318ebce5
UVKUZNETSOV <yuriy_kuznetsov_73@mail.ru>
2023-10-06 02:34:12 +0300

    ft upd

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,45 +1,21 @@
-import './style/style.css';
-import { useEffect } from 'react';
-import logo from './img/main-logo.png'
-import background from './img/background.png'
-
-const google = window.google;
-
-const handleCallbackResponse = (response) => {
-  console.log("Encoded token: " + response.credential)
-}
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Homepage } from './components/Homepage';
+import { Dashboard } from './components/Dashboard';
 
 function App() {
-
-  useEffect(() => {
-    google.accounts.id.initialize({
-      client_id: "1083641433679-1m22s1mpo5d1muio0aannc0kh2ofivbu.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    })
-
-    google.accounts.id.renderButton(
-      document.getElementById("sign-in-div"),
-      { theme: "outline", size: "large" }
-    )
-
-  }, []);
-
-  return (
-    <div className="App" style={{ backgroundImage: `url(${background})`}}>
-      <main>
-        <img src={logo} alt="Logo" className="app__img"/>
-        <h1 className="app__header">
-          Google Fit Tracker 
-          <br/>
-          <span>
-            v1.0
-          </span>
-        </h1>
-        <div id="sign-in-div"></div>
-      </main>
-    </div>
-  );
-
+  return(
+    <>
+      {/* <header>
+        <a href="/">Home</a>
+        <a href="/dashboard">Dashboard</a>
+      </header> */}
+      <Routes>
+        <Route exact path="/" element={<Homepage />}/>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
